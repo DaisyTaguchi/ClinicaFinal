2a36c83c82a3107f66eb094f720382803c4600fb
DaisyTaguchi <83254640+DaisyTaguchi@users.noreply.github.com>
2021-11-15 20:35:10 -0300

    Add files via upload

---
+++ ClinicaAtualizado/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoClinica.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoClinica.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<MMedico> TBMedico { get; set; }
+
+        public DbSet<MSecretaria> TBSecretaria { get; set; }
+
+        public DbSet<MLogin> TBLogin { get; set; }
+
+        public DbSet<MPaciente> TBPaciente { get; set; }
+        public DbSet<MConsulta> TBConsulta { get; set; }
+    }
+
+}
